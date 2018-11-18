c8093dcf93ca7fe0ed50fe436fc5b4a020280554
mishel <s_mishel_s@ukr.net>
2018-11-18 22:25:08 +0200

    Updates

--- precache-manifest.3745bdf6fe295be2f2c23bde5d4bbfe7.js
+++ precache-manifest.3745bdf6fe295be2f2c23bde5d4bbfe7.js
@@ -4,12 +4,12 @@ self.__precacheManifest = [
     "url": "/epizodeslist_with_calendar/static/media/tv-png.332c960b.png"
   },
   {
-    "revision": "d5562389fb239819de4d",
+    "revision": "96f186c5f1f8dcc422ea",
     "url": "/epizodeslist_with_calendar/static/css/main.6f275ecd.chunk.css"
   },
   {
-    "revision": "f5331cb6372b6c0d8baf2dd7e200498c",
-    "url": "/epizodeslist_with_calendar/static/media/OpenSans-Bold.f5331cb6.ttf"
+    "revision": "e1c83f9474e0cc1d84a13c6d1ddf3ca5",
+    "url": "/epizodeslist_with_calendar/static/media/OpenSans-SemiBold.e1c83f94.ttf"
   },
   {
     "revision": "09a61e416e9d430c9af3",
@@ -20,8 +20,8 @@ self.__precacheManifest = [
     "url": "/epizodeslist_with_calendar/static/js/runtime~main.cff55479.js"
   },
   {
-    "revision": "d5562389fb239819de4d",
-    "url": "/epizodeslist_with_calendar/static/js/main.d5562389.chunk.js"
+    "revision": "96f186c5f1f8dcc422ea",
+    "url": "/epizodeslist_with_calendar/static/js/main.96f186c5.chunk.js"
   },
   {
     "revision": "9ff12f694e5951a6f51a9d63b05062e7",
@@ -32,15 +32,15 @@ self.__precacheManifest = [
     "url": "/epizodeslist_with_calendar/static/media/OpenSans-Regular.d7d5d458.ttf"
   },
   {
-    "revision": "e1c83f9474e0cc1d84a13c6d1ddf3ca5",
-    "url": "/epizodeslist_with_calendar/static/media/OpenSans-SemiBold.e1c83f94.ttf"
+    "revision": "f5331cb6372b6c0d8baf2dd7e200498c",
+    "url": "/epizodeslist_with_calendar/static/media/OpenSans-Bold.f5331cb6.ttf"
   },
   {
     "revision": "09a61e416e9d430c9af3",
     "url": "/epizodeslist_with_calendar/static/css/1.b9779858.chunk.css"
   },
   {
-    "revision": "ef5f5ad33d98396b0f02b25df847c252",
+    "revision": "6a3aeb43481f95ce330aa2254c75ae60",
     "url": "/epizodeslist_with_calendar/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/epizodeslist_with_calendar/precache-manifest.11eb2d41f4525901b3c0432a40455357.js"
+  "/epizodeslist_with_calendar/precache-manifest.3745bdf6fe295be2f2c23bde5d4bbfe7.js"
 );
 
 workbox.clientsClaim();
