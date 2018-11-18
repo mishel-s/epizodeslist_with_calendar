d00066cd87b001d57402a3037a40cdb95bcd756a
mishel <s_mishel_s@ukr.net>
2018-11-18 21:30:46 +0200

    Updates

---
+++ precache-manifest.87a2415b914c4900ecef229cb1dca863.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "332c960b2394866eb21704dcb8259363",
+    "url": "/epizodeslist_with_calendar/static/media/tv-png.332c960b.png"
+  },
+  {
+    "revision": "0204cb0670465bd29a72",
+    "url": "/epizodeslist_with_calendar/static/css/main.6f275ecd.chunk.css"
+  },
+  {
+    "revision": "f5331cb6372b6c0d8baf2dd7e200498c",
+    "url": "/epizodeslist_with_calendar/static/media/OpenSans-Bold.f5331cb6.ttf"
+  },
+  {
+    "revision": "09a61e416e9d430c9af3",
+    "url": "/epizodeslist_with_calendar/static/js/1.09a61e41.chunk.js"
+  },
+  {
+    "revision": "cff55479a6503b6b10fc",
+    "url": "/epizodeslist_with_calendar/static/js/runtime~main.cff55479.js"
+  },
+  {
+    "revision": "0204cb0670465bd29a72",
+    "url": "/epizodeslist_with_calendar/static/js/main.0204cb06.chunk.js"
+  },
+  {
+    "revision": "9ff12f694e5951a6f51a9d63b05062e7",
+    "url": "/epizodeslist_with_calendar/static/media/OpenSans-Light.9ff12f69.ttf"
+  },
+  {
+    "revision": "d7d5d4588a9f50c99264bc12e4892a7c",
+    "url": "/epizodeslist_with_calendar/static/media/OpenSans-Regular.d7d5d458.ttf"
+  },
+  {
+    "revision": "e1c83f9474e0cc1d84a13c6d1ddf3ca5",
+    "url": "/epizodeslist_with_calendar/static/media/OpenSans-SemiBold.e1c83f94.ttf"
+  },
+  {
+    "revision": "09a61e416e9d430c9af3",
+    "url": "/epizodeslist_with_calendar/static/css/1.b9779858.chunk.css"
+  },
+  {
+    "revision": "33f79d80fb2adb825e477484f201957c",
+    "url": "/epizodeslist_with_calendar/index.html"
+  }
+];
